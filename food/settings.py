9555c87dcc73d06a2db3fa327fdffadf54a157bb
vaibhavdih <vaibhav29122000@gmail.com>
2020-05-07 06:18:56 +0530

    first commit

--- food/settings.py
+++ food/settings.py
@@ -118,6 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/state/'
 STATIC_ROOT=os.path.join(BASE_DIR,'state/')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
